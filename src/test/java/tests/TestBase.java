3e47d59cbe1722f8931d5a4e313b07a71f630879
IlyaKoch <kochetkov6363@gmail.com>
2023-02-21 17:46:34 +0400

    added config video enabled

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,7 +1,9 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -9,12 +11,14 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.browserSize = "2100x1080";
         Configuration.baseUrl = "https://demoqa.com";
 //        Настройки для selenoid
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
     }
 
